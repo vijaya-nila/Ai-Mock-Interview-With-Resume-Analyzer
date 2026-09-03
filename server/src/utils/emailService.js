13253d178faa115bd0e6e90651dd64d45a5e3381
viji-dev <vijisubu2314@gmail.com>
2026-09-03 20:01:08 +0530

    Fix Gmail SMTP connection

--- server/src/utils/emailService.js
+++ server/src/utils/emailService.js
@@ -14,11 +14,11 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-
 });
 
 const sendVerificationEmail = async (email, token) => {
