9d25e7efec25cc61d2d8b8cb926981321fa4db03
viji-dev <vijisubu2314@gmail.com>
2026-09-04 10:31:35 +0530

    Replace Resend with Brevo email API 3

--- server/src/utils/emailService.js
+++ server/src/utils/emailService.js
@@ -1,10 +1,12 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
